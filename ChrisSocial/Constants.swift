93255f46a1cbc42aec9eacd412ea39a591871168
Christos Karolemeas <karolemeaschris@gmail.com>
2016-11-22 14:29:34 +0200

    Sign in ui custom classes complete

---
+++ ChrisSocial/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  ChrisSocial
+//
+//  Created by Christos Karolemeas on 22/11/2016.
+//  Copyright © 2016 Christos Karolemeas. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 125.0 / 255.0
+
+
---
+++ ChrisSocial/RoundBtn.swift
@@ -0,0 +1,28 @@
+//
+//  RoundBtn.swift
+//  ChrisSocial
+//
+//  Created by Christos Karolemeas on 22/11/2016.
+//  Copyright © 2016 Christos Karolemeas. All rights reserved.
+//
+
+import UIKit
+
+class RoundBtn: UIButton {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        imageView?.contentMode = .scaleAspectFit
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        layer.cornerRadius = self.frame.width / 2
+    }
+}
