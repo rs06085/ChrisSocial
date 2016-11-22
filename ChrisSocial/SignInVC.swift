6e314e6bdf8a3118f66facff6b6b35313c77e813
Christos Karolemeas <karolemeaschris@gmail.com>
2016-11-22 11:24:15 +0200

    Completed setting up Firebse integration

---
+++ ChrisSocial/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  SignInVC.swift
+//  ChrisSocial
+//
+//  Created by Christos Karolemeas on 22/11/2016.
+//  Copyright © 2016 Christos Karolemeas. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
