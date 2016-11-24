25411193b16d44bb75828b66183695a97feab7cf
Christos Karolemeas <karolemeaschris@gmail.com>
2016-11-24 12:50:27 +0200

    Social feed UI completed

--- ChrisSocial/CircleImageView.swift
+++ ChrisSocial/CircleImageView.swift
@@ -1,22 +1,25 @@
 //
-//  FancyView.swift
+//  CircleImageView.swift
 //  ChrisSocial
 //
-//  Created by Christos Karolemeas on 22/11/2016.
+//  Created by Christos Karolemeas on 23/11/2016.
 //  Copyright © 2016 Christos Karolemeas. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
-
+class CircleImageView: UIImageView {
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = self.frame.width / 2
     }
 }
